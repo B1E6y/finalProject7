478ba6a8cde8064c5f9d587e370527747d81ba96
B1E6y <96926026+B1E6y@users.noreply.github.com>
2025-03-13 13:04:04 +0200

    Update basic.js

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -16,67 +16,55 @@ const revenueInternPage = require('../page/revenueIntern.page.js')
     it('Go to github.com', async () => {
         await browser.url('https://github.com/');
         scrollCareersPage.scrollToCareers();
-        await browser.pause(1000);
+        
         })
 
     it('Click on Open Source', async () => {
         clickOnCareersPage.clickOnCareersButton();
-        await browser.pause(1000);
         })
 
     it('Click on Topics', async () => {
         rolesPage.clickRolesButton();
-        await browser.pause(3000);
         })
 
     it('[1]Output result', async () => {
         staffSoftwareEngineerPage.staffSoftwareEngineerText();
-        await browser.pause(1000);
         }) 
 
      it('[2]Output result', async () => {
         incidentResponseAnalystProductSecurityPage.incidentResponseAnalystProductSecurityText();
-        await browser.pause(1000);
         })
 
      it('[3]Output result', async () => {
         seniorCustomerSuccessArchitectPage.seniorCustomerSuccessArchitectText();
-        await browser.pause(1000);
         })
 
     it('[4]Output result', async () => {
         securityAnalystIIIPage.securityAnalystIIIText();
-        await browser.pause(1000);
         })
 
     it('[5]Output result', async () => {
         seniorCustomerSuccessManagerPage.seniorCustomerSuccessManagerText();
-        await browser.pause(1000);
         })  
 
     it('[6]Output result', async () => {
         directorGlobalBenefitsPage.directorGlobalBenefitsText();
-        await browser.pause(1000);
         })
 
     it('[7]Output result', async () => {
         staffManagerIncidentResponsePage.staffManagerIncidentResponseText();
-        await browser.pause(1000);
         })
 
     it('[8]Output result', async () => {
         incidentResponseAnalystProductSecurity1Page.incidentResponseAnalystProductSecurity1Text();
-        await browser.pause(1000);
         })
 
     it('[9]Output result', async () => {
         staffProductManagerPage.staffProductManagerText();
-        await browser.pause(1000);
         }) 
 
     it('[10]Output result', async () => {
         revenueInternPage.revenueInternPageText();
-        await browser.pause(3000);
         }) 
                                                     
     
